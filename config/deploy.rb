fb3dd524be00da55642f14ac4b0820eb6b04cd3c
tevans <tevans@g-and-o.com>
2011-10-11 09:10:03 -0400

    Adding deployment settings.

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+set :application, "live_transit_api"
+set :repository,  "git@github.com:ipublic/live_transit_api.git"
+set :deploy_to, "/var/www/live_transit_api"
+
+set :scm, :git
+set :use_sudo, false
+set :deploy_via, :copy
+set :user, :root
+set :keep_releases, 2
+
+role :web, "107.20.248.237"
+role :app, "107.20.248.237"
+role :db, "107.20.248.237", :primary => true
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+     run "rvm rvmrc trust #{File.join(current_release)}"
+     run "chown apache:apache #{File.join(current_path,'..','..')} -R"
+     run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
