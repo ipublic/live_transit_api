2003de2a21daca05eef975a7fd056ae9922b88e1
tevans <tevans@g-and-o.com>
2011-10-14 12:57:05 -0400

    Minor view fixup.

--- couch_docs/_design/StopTime/views/by_trip_geometry/map.js
+++ couch_docs/_design/StopTime/views/by_trip_geometry/map.js
@@ -2,8 +2,8 @@ function(doc) {
   if (doc.type && doc.type == "StopTime") {
     emit(doc.trip_id, {
         "stop_sequence": doc.stop_sequence,                                                       
-        "lat": doc.geometry.coordinates[1],                                                       
-        "lon": doc.geometry.coordinates[0]                                                        
+        "lat": doc.stop_geometry.coordinates[1],                                                       
+        "lon": doc.stop_geometry.coordinates[0]                                                        
       }); 
   }
 }
