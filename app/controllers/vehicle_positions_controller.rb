57dd450cefc7d46ad9a110f5290508f31620a069
tevans <tevans@g-and-o.com>
2012-01-23 14:28:12 -0500

    Data now collected properly.

--- app/controllers/vehicle_positions_controller.rb
+++ app/controllers/vehicle_positions_controller.rb
@@ -1,11 +1,9 @@
 class VehiclePositionsController < ApplicationController
   def create
-    @vehicle_position = VehiclePosition.create_or_update(params[:NewDataSet][:Table])
+    @vehicle_position = VehiclePosition.create_or_update(params)
 
     respond_to do |format|
-      format.html { render :json => @vehicle_position, :content_type => "application/json", :status => :ok }
-      format.xml { render :xml => @vehicle_position.to_xml({:dasherize => false}), :status => :ok }
-      format.json { render :json => @vehicle_position, :status => :ok }
+      format.xml { render :nothing => true, :status => 202 }
     end
   end
 end
--- app/models/vehicle_position.rb
+++ app/models/vehicle_position.rb
@@ -5,20 +5,11 @@ class VehiclePosition < CouchRest::Model::Base
   property :speed, String
   property :heading, String
   property :vehicle_position_date_time, String
-  property :route_direction, String
-  property :route_id, String
   property :trip_id, String
-  property :incident_desc, String
   property :last_stop_deviation, String
   property :predicted_deviation, String
-  property :previous_stop_code, String
-  property :next_stop_code, String
-  property :next_scheduled_stop_time, String
   property :previous_sequence, Integer
   property :next_sequence, Integer
-  property :incident_date_time, String
-  property :last_scheduled_time, String
-  property :status_scheduled_time, String
 
   design do
     view :by_vehicle_id
@@ -28,13 +19,31 @@ class VehiclePosition < CouchRest::Model::Base
     ActiveModel::Serializers::Xml::Serializer.new(self, options).serialize(&block)
   end
 
-  def self.create_or_update(props)
-    rec_id = props['vehicle_id']
-    record = self.by_vehicle_id.key(rec_id).first
-    if record.nil?
-      record = self.create!(props)
+  def self.create_or_update_many(props)
+    if props.kind_of?(Array)
+      props.map do |prop|
+        create_or_update(prop)
+      end.last
     else
-      record.update_attributes(props)
+      create_or_update(props)
+    end
+  end
+
+  def self.create_or_update(params)
+    record = nil
+    if !params.nil?
+      if !params["NewDataSet"].nil?
+        if !params["NewDataSet"]["Table"].nil?
+          props = params["NewDataSet"]["Table"]
+          rec_id = props['vehicle_id']
+          record = self.by_vehicle_id.key(rec_id).first
+          if record.nil?
+            record = self.create!(props)
+          else
+            record.update_attributes(props)
+          end
+        end
+      end
     end
     record
   end
