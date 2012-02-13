8a47677a11934bad31a008d9c66ed294557bb6eb
tevans <tevans@g-and-o.com>
2012-02-13 15:20:34 -0500

    Going back to old way of creating busses.

--- app/models/vehicle_position.rb
+++ app/models/vehicle_position.rb
@@ -71,19 +71,16 @@ class VehiclePosition < CouchRest::Model::Base
           end
           self.database.bulk_save(existing_vehicles)
           new_vehicle_ids = vehicle_data_hash.keys.reject { |k| existing_vehicle_ids.include?(k) }
-          if new_vehicle_ids.length > 0
-            new_vps = new_vehicle_ids.map do |nvid|
-              VehiclePosition.new(vehicle_data_hash[nvid])
-            end
-            self.database.bulk_save(new_vps)
+          new_vehicle_ids.create do |nvid|
+            VehiclePosition.create!(vehicle_data_hash[nvid])
           end
         end
       end
     end
-  end
+end
 
-  def to_param
-    vehicle_id
-  end
+def to_param
+  vehicle_id
+end
 
 end
