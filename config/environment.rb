a2eefea5e8b1eaaa4cef36e4271679710afbffaa
tevans <tevans@g-and-o.com>
2012-02-17 14:08:40 -0500

    Fixing connection issues.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 LiveTransitApi::Application.initialize!
+
+if defined?(PhusionPassenger)
+    PhusionPassenger.on_event(:starting_worker_process) do |forked|
+      # Reset Rails's object cache
+      # Only works with DalliStore
+      Rails.cache.reset if forked
+    end
+end
