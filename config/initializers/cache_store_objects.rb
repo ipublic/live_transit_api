4b995999b87402eb5522b527160b90fd28cf699f
tevans <tevans@g-and-o.com>
2012-02-17 11:12:15 -0500

    Now should work in production.

--- config/initializers/cache_store_objects.rb
+++ config/initializers/cache_store_objects.rb
@@ -10,9 +10,26 @@ module Dalli
       perform(:add, key, Marshal.dump(value), ttl, options)
     end
 
+    def cas(key, ttl=nil, options=nil, &block)
+      ttl ||= @options[:expires_in]
+      (value, cas) = perform(:cas, key)
+      value = (!value || value == 'Not found') ? nil : value
+      if value
+        newvalue = block.call(value)
+        perform(:set, key, Marshal.dump(newvalue), ttl, cas, options)
+      end
+    end
+
+    def set(key, value, ttl=nil, options=nil)
+      raise "Invalid API usage, please require 'dalli/memcache-client' for compatibility, see Upgrade.md" if options == true
+      ttl ||= @options[:expires_in]
+      perform(:set, key, Marshal.dump(value), ttl, 0, options)
+    end
+
+
     def get(key, options=nil)
       resp = perform(:get, key)
-      (!resp || resp == 'Not found') ? nil : Marshal.load(resp)
+      (!resp || resp == 'Not found') ? nil : resp
     end
   end
 end
