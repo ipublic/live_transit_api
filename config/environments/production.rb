82de37ef1b2645f95dab933dd3301748f1904526
tevans <tevans@g-and-o.com>
2012-02-17 10:42:03 -0500

    Fixing issue again.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,12 +59,3 @@ LiveTransitApi::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 end
-if defined?(PhusionPassenger)
-  PhusionPassenger.on_event(:starting_worker_process) do |forked|
-    # Only works with DalliStore
-    Rails.cache.reset if forked
-    # Reset Rails's session store
-    # If you know a cleaner way to find the session store instance, please let me know
-    ObjectSpace.each_object(ActionDispatch::Session::DalliStore) { |obj| obj.reset }
-  end
-end
