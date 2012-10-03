44495967ea41fe884bc0f4a5735be801fda3698f
tevans <tevans@g-and-o.com>
2012-10-03 15:10:45 -0400

    Adding pg sql

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'haml', '3.1.3'
 gem 'multi_json', '1.0.4'
 gem 'dalli'
 gem 'ruby-protocol-buffers'
+gem 'pg', '0.14.1'
 
 # Bundle edge Rails instead:
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
---
+++ db/migrate/20121003190001_create_agencies.rb
@@ -0,0 +1,20 @@
+class CreateAgencies < ActiveRecord::Migration
+  def up
+    create_table :agencies do |t|
+      t.string :agency_id, :limit => 255, :null => true
+      t.string :agency_name, :limit => 512, :null => false
+      t.string :agency_url, :limit => 1024, :null => false
+      t.string :agency_timezone, :limit => 255, :null => false
+      t.string :agency_lang, :limit => 255, :null => true
+      t.string :agency_phone, :limit => 255, :null => true
+      t.string :agency_fare_url, :limit => 1024, :null => true
+    end
+
+    add_index :agencies, :agency_id
+    add_index :agencies, :agency_name
+  end
+
+  def down
+    drop_table :agencies
+  end
+end
