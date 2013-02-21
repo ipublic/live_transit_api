9f2946ceb10d1f790a9ddf0fe582da7ae2a4ee02
tevans <tevans@g-and-o.com>
2013-02-21 15:14:32 -0500

    Apparently forgot this one.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery
+  after_filter :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = "*"
+  end
 
 
   def process_not_found
