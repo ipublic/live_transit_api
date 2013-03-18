dcac18fbc7e2d8a6e873fe0edf6245554c3e0ddc
tevans <tevans@g-and-o.com>
2013-03-18 11:54:59 -0400

    Route resources are fixed.

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,3 +1,16 @@
 class Route < ActiveRecord::Base
   has_many :trips, :foreign_key => "route_id", :primary_key => "route_id"
+
+  scope :for_route_id, lambda { |r_id|
+    where(:route_id => r_id).
+      includes(:trips => [:shape_points, { :stop_times => :stop }])
+  }
+
+  def shapes
+    trips.map(&:geometry)
+  end
+
+  def self.by_route_id(r_id)
+    self.for_route_id(r_id).first
+  end
 end
