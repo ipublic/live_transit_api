191b2c166c29809e36985d723c3dc71d31f2233c
tevans <tevans@g-and-o.com>
2013-03-20 08:48:44 -0400

    Fixed routes.

--- app/controllers/gtfs_realtime_controller.rb
+++ app/controllers/gtfs_realtime_controller.rb
@@ -1,5 +1,5 @@
 class GtfsRealtimeController < ApplicationController
-  before_filter :authenticate_user!
+  # before_filter :authenticate_user!
   def show
     @lookup_time, @gtfs_realtime = Gtfs::RealtimeFeed.fetch
 
--- app/models/route.rb
+++ app/models/route.rb
@@ -2,12 +2,16 @@ class Route < ActiveRecord::Base
   has_many :trips, :foreign_key => "route_id", :primary_key => "route_id"
 
   scope :for_route_id, lambda { |r_id|
-    where(:route_id => r_id).
-      includes(:trips => [:shape_points, { :stop_times => :stop }])
+      includes(:trips => [:shape_points, { :stop_times => :stop }]).
+        where(:route_id => r_id)
   }
 
   def shapes
-    trips.map(&:geometry)
+    # Fix this once we move to 1.9.3
+    # trips.uniq(&:shape_id).map(&:geometry)
+    (trips.inject([]) do |memo, trip|
+      memo.map(&:shape_id).include?(trip.shape_id) ? memo : memo + [trip]
+    end).map(&:geometry)
   end
 
   def self.by_route_id(r_id)
