daa7726d20faef70754ed03ab814531ea118224f
Dan Thomas <dan.thomas@ipublic.org>
2013-04-12 18:46:37 -0400

    Fix: change spatial column name to: geometry

--- app/models/features_stop.rb
+++ app/models/features_stop.rb
@@ -1,6 +1,18 @@
+<<<<<<< HEAD:app/models/features_stop.rb
 class FeaturesStop < ActiveRecord::Base
   attr_accessible :stop_id, :stop_latlon, :stop_code, :stop_name, :stop_desc, :zone_id, :stop_url, 
                   :location_type, :parent_station, :stop_timezone, :wheelchair_boarding
                   
   has_many :stop_times, :foreign_key => "stop_id", :primary_key => "stop_id"
+=======
+class Features::Stop < ActiveRecord::Base
+  attr_accessible :stop_id, :geometry, :stop_code, :stop_name, :stop_desc, :zone_id, :stop_url, 
+                  :location_type, :parent_station, :stop_timezone, :wheelchair_boarding
+                  
+  has_many :stop_times, :foreign_key => "stop_id", :primary_key => "stop_id"
+  set_rgeo_factory_for_column(:geometry, RGeo::Geographic.spherical_factory(:srid => 4326))
+  
+  default_scope joins(:stop_times)  # Eager load -- if it makes sense?
+  
+>>>>>>> Fix: change spatial column name to: geometry:app/models/features/stop.rb
 end
