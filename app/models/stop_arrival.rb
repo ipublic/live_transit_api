cb953d0ef709dff8f1e70b3eb4a2f86ca50f7b18
tevans <tevans@g-and-o.com>
2013-05-13 18:07:50 -0400

    Calculate ALL the stops.

--- app/models/stop_arrival.rb
+++ app/models/stop_arrival.rb
@@ -16,6 +16,7 @@ class StopArrival
       memo[t.trip_id] = t.block_id
       memo
     end
+    block_ids = vp_block_map.values.uniq
     vp_blocks = vp.inject({}) do |memo, vp|
       memo[vp_block_map[vp.trip_id]] = vp
       memo
@@ -23,8 +24,15 @@ class StopArrival
     max_dev = ((vp.map { |v| v.predicted_deviation.abs }.max) || 0) * 60
     start_time = calc_time.to_i - max_dev - 30
     end_time = (calc_time + 2.hours).to_i
-    all_events = StopTimeEvent.for_arrival_time_between(start_time, end_time)
-    all_events.map { |ae| StopArrival.new(ste, vp_block_map[ste.trip_id]) }.reject(&:vehicle_has_passed)
+    now_cutoff = calc_time.to_i - 10
+    all_events = StopTimeEvent.for_arrival_time_with_blocks(now_cutoff, start_time, end_time, block_ids)
+    all_events.inject([]) do |memo,ste| 
+      stop_arr = StopArrival.new(ste, vp_blocks[ste.block_id])
+      if !(stop_arr.vehicle_has_passed || stop_arr.happened_before(now_cutoff))
+        memo << stop_arr
+      end
+      memo
+    end
   end
 
   def initialize(ste, vehicle)
@@ -32,13 +40,13 @@ class StopArrival
     @route_long_name = ste.route.route_long_name
     @route_short_name = ste.route.route_short_name
     @route_id = ste.route.id
-    @trip_id = ste.trip.trip_id
-    @trip_headsign = ste.trip.trip_headsign
+    @trip_id = ste.stop_time.trip.trip_id
+    @trip_headsign = ste.stop_time.trip.trip_headsign
     @stop_code = ste.stop.stop_code
     @stop_id = ste.stop_id
     @scheduled_arrival = ste.arrival_time
     @scheduled_departure = ste.departure_time
-    @is_calculated = !vehicle.null?
+    @is_calculated = !vehicle.nil?
     @arrival_time = ste.arrival_time
     @departure_time = ste.departure_time
     @vehicle_has_passed = false
@@ -46,8 +54,11 @@ class StopArrival
       @vehicle_id = vehicle.vehicle_id
       @arrival_time = ste.arrival_time - (vehicle.predicted_deviation * 60)
       @departure_time = ste.departure_time - (vehicle.predicted_deviation * 60)
-      @vehicle_has_passed = (ste.trip.trip_id == vehicle.trip_id) && (ste.stop_time.stop_sequence < vehicle.previous_sequence) 
+      @vehicle_has_passed = (ste.stop_time.trip.trip_id == vehicle.trip_id) && (ste.stop_time.stop_sequence < (vehicle.previous_sequence || 0)) 
     end
   end
 
+  def happened_before(now_cutoff)
+    @arrival_time < now_cutoff
+  end
 end
--- app/models/stop_time_event.rb
+++ app/models/stop_time_event.rb
@@ -1,5 +1,5 @@
 class StopTimeEvent < ActiveRecord::Base
-  belongs_to :stop_time, :foreign_key => "stop_time_id", :inverse_of => :stop_time_events
+  belongs_to :stop_time, :foreign_key => "stop_time_id", :inverse_of => :stop_time_events, :include => {:trip => :route}
   belongs_to :stop, :foreign_key => "stop_id", :primary_key => "stop_id"
 
   delegate :route, :to => :stop_time
@@ -8,24 +8,31 @@ class StopTimeEvent < ActiveRecord::Base
   delegate :block_id, :to => :stop_time
 
   scope :for_stop_and_arrival_time, lambda { |s_id, t_val|
-    where("stop_time.stop_sequence <> trip.last_stop_sequence and stop_id = ? and arrival_time > ?", s_id, t_val).
-      order("arrival_time").
-      includes(:stop_time => [:stop, { :trip => :route }])
+    where("stop_times.stop_sequence <> trip.last_stop_sequence and stop_id = ? and stop_time_events.arrival_time > ?", s_id, t_val).
+      order("stop_time_events.arrival_time").
+      joins(:stop, {:stop_time => [:stop, { :trip => :route }]})
   }
 
-  scope :for_arrival_time_between, lambda { |s_val, e_val|
-    where("stop_time.stop_sequence <> trip.last_stop_sequence and arrival_time > ? and departure_time < ?", s_val, e_val).
-      order("arrival_time").
-      includes(:stop_time => [:stop, { :trip => :route }])
+  scope :for_arrival_time_with_blocks, lambda { |n_val, s_val, e_val, block_ids|
+    joins({:stop_time => [{ :trip => :route }, :stop]}, :stop).
+      where("stop_times.stop_sequence <> trips.last_stop_sequence and (
+            (stop_time_events.arrival_time > ? and stop_time_events.arrival_time < ?)
+            OR (
+            (stop_time_events.arrival_time > ? and stop_time_events.arrival_time < ?)
+            AND
+            (trips.block_id in (?))
+            ))", n_val, e_val, s_val, e_val, block_ids).
+    includes({:stop_time => [{ :trip => :route }, :stop]}, :stop).
+      order("stop_time_events.arrival_time")
   }
 
   scope :for_stop_and_blocks_between, lambda { |s_id, b_ids, s_val, e_val|
-    where("stop_time.stop_sequence <> trip.last_stop_sequence and stop_time_events.stop_id = ? and trips.block_id in (?) and stop_time_events.arrival_time > ? and stop_time_events.departure_time < ?", s_id, b_ids, s_val, e_val).
+    where("stop_times.stop_sequence <> trips.last_stop_sequence and stop_time_events.stop_id = ? and trips.block_id in (?) and stop_time_events.arrival_time > ? and stop_time_events.departure_time < ?", s_id, b_ids, s_val, e_val).
       joins(:stop_time => [:stop, { :trip => :route }])
   }
 
   scope :for_blocks_between, lambda { |b_ids, s_val, e_val|
-    where("stop_time.stop_sequence <> trip.last_stop_sequence and trips.block_id in (?) and stop_time_events.arrival_time > ? and stop_time_events.arrival_time < ?", b_ids, s_val, e_val).
+    where("stop_times.stop_sequence <> trips.last_stop_sequence and trips.block_id in (?) and stop_time_events.arrival_time > ? and stop_time_events.arrival_time < ?", b_ids, s_val, e_val).
       joins(:stop_time => [:stop, { :trip => :route }])
   }
 end
